6fb82915299c140c4612fb4b2341d4f975a49dd0
Valentin Orban <valentin@agence-webup.com>
2021-09-06 13:37:18 +0200

    feat(install.sh): added colors

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+
+ESC="\033["
+BLACK="${ESC}30m"
+RED="${ESC}31m"
+GREEN="${ESC}32m"
+YELLOW="${ESC}33m"
+BLUE="${ESC}34m"
+MAGENTA="${ESC}35m"
+CYAN="${ESC}36m"
+WHITE="${ESC}37m"
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 target_folder="/usr/local/bin"
@@ -6,7 +16,7 @@ target_folder="/usr/local/bin"
 pushd "$SCRIPTPATH" >/dev/null 2>&1
 
 for file in $(ls ./scripts); do
-    echo "- installing [$file]"
+    echo "- installing [${GREEN}$file${WHITE}]"
     chmod +x ./scripts/$file
     ln -sf $SCRIPTPATH/scripts/$file $target_folder/$file
 done
